c7e855255dd6ccf872720256051c342b0596d16c
Christoph Aldrian <caldrian@users.noreply.github.com>
2022-01-10 15:48:56 +0000

    Remove USE_MYMATH

--- Shared/MathFunctions/MathFunctions.cxx
+++ Shared/MathFunctions/MathFunctions.cxx
@@ -1,15 +1,8 @@
 #include "MathFunctions.h"
 #include <math.h>
-
-#ifdef USE_MYMATH
-#  include "mysqrt.h"
-#endif
+#include "mysqrt.h"
 
 double mathfunctions_sqrt(double x)
 {
-#ifdef USE_MYMATH
   return mysqrt(x);
-#else
-  return sqrt(x);
-#endif
 }
