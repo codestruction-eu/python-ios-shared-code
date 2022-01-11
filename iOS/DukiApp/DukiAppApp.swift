8cca0ad7affa658f3a5ae4c5aa9e1eedb20b1b25
Christoph Aldrian <caldrian@users.noreply.github.com>
2022-01-11 07:32:26 +0100

    Create iOS app

---
+++ iOS/DukiApp/DukiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DukiAppApp.swift
+//  DukiApp
+//
+//  Created by Christoph Aldrian on 11.01.22.
+//
+
+import SwiftUI
+
+@main
+struct DukiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
